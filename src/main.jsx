c6170beeb8cbc622fd522e52f45f9831463d6464
Abdullah Ahmad <theneweratech@gmail.com>
2024-09-03 03:22:08 +0500

    styles re-organized

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
+// Styles
+import '../styles/main.scss';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
--- vite.config.js
+++ vite.config.js
@@ -12,4 +12,9 @@ export default defineConfig({
     assetsDir: '.',
     // brotliSize: false,
   },
+  server: {
+    watch: {
+      usePolling: true,
+    }
+  }
 })
